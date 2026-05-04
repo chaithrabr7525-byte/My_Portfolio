98981bb6dc4ff2068c68ca3ac5f538c6f33ddc86
chaithrabr7525-byte <chaithrabr7525@gmail.com>
2026-05-04 10:18:33 +0530

    data

---
+++ js/data/projects.js
@@ -0,0 +1,46 @@
+const projectsData = [
+  {
+    id: 1,
+    name: "BookMyShow Clone",
+    category: "MERN",
+    description:
+      "Full stack movie ticket booking application with AI-powered smart movie suggestions.",
+    technologies: ["ReactJS", "ExpressJS", "MongoDB", "NodeJS"],
+    status: "Demo",
+    liveDemo: "#",
+    github: "#"
+  },
+  {
+    id: 2,
+    name: "Medical AI Chatbot",
+    category: "MERN",
+    description:
+      "AI-powered chatbot for providing medical information and assistance.",
+    technologies: ["HTML","CSS","JS"],
+    status: "Live",
+    liveDemo: "#",
+    github: "#"
+  },
+  {
+    id: 3,
+    name: "Portfolio Website",
+    category: "Frontend",
+    description:
+      "Responsive personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "Tailwind CSS", "JavaScript"],
+    status: "Live",
+    liveDemo: "#",
+    github: "#"
+  },
+  {
+    id: 4,
+    name: "Fetal Cerebellum in 2D Ultrasound",
+    category: "Python",
+    description:
+      "AI-powered tool for analyzing fetal cerebellum in 2D ultrasound images to assist in prenatal diagnosis.",
+    technologies: ["Python", "OpenCV", "NumPy"],
+    status: "Live",
+    liveDemo: "#",
+    github: "#"
+  }
+];
